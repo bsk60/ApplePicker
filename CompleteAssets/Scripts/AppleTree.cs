3abcaf84794c338c4d92251731e63541f4155754
bsk60 <brendonkang@Brendons-MBP.home>
2015-12-16 15:52:30 -0500

    Complete Apple Picker
    Score Bugged though

---
+++ CompleteAssets/Scripts/AppleTree.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleTree : MonoBehaviour {
+
+	public GameObject applePrefab;
+	public float speed = 1f;
+	public float leftAndRightEdge = 10f;
+	public float chanceToChangeDirections = 0.1f;
+	public float secondsBetweenAppleDrops = 1f;
+
+	void Start(){
+		//Dropping apples every second
+		InvokeRepeating ("DropApple", 2f, secondsBetweenAppleDrops);
+	}
+	void DropApple(){
+		GameObject Apple = Instantiate (applePrefab) as GameObject;
+		Apple.transform.position = transform.position;
+	}
+
+	void Update(){
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed); //Move Right
+		} else if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs (speed); //Move Left
+		} 
+	}
+
+	void FixedUpdate(){
+		if (Random.value < chanceToChangeDirections) {
+			speed *=-1; //Change Direction
+		}
+	}
+}
---
+++ CompleteAssets/Scripts/Basket.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour {
+	//public Text scoreGT;
+
+	// Update is called once per frame
+	void Update () {
+	//Get the current screen position of the mouse from Input
+		Vector3 mousePos2D = Input.mousePosition;
+	//The Camera's z position sets the how far to push the mouse into 3D
+		mousePos2D.z = -Camera.main.transform.position.z;
+	//Convert the point from 2D screen space into 3D game world space
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+	//Move the x position of this Basket to the x position of the Mouse 
+		Vector3 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		this.transform.position = pos;
+	}
+	
+	void Start(){
+		//Find a reference to the ScoreCounter GameObject
+		//GameObject scoreGO = GameObject.Find ("ScoreCounter");
+		//Get the GUIText Component of that GameObject
+		//scoreGT = scoreGO.GetComponent(Text);
+			//scoreGT.text ="0";
+	}
+	void OnCollisionEnter(Collision coll){
+		GameObject collidedWith = coll.gameObject;
+		if (collidedWith.tag == "Apple") {
+			Destroy (collidedWith);
+		}
+		//int score = int.Parse (scoreGT.text);
+		//score += 100;
+		//scoreGT.text = score.ToString ();
+	}
+	}
+
